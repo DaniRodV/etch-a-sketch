830d4eaa73f9c03aa427ee5d081ed494c1aaa11c
Daniel R <174905798+DaniRodV@users.noreply.github.com>
2024-08-20 13:40:47 -0400

    Add functionality to etch a sketch

--- script.js
+++ script.js
@@ -1,12 +1,19 @@
-function createGrid () {
-    for (let i = 1; i <= 273; i++) {
-        const gridContainer = document.querySelector("#grid-container");
-        const gridSquare = document.createElement("div");
-        gridSquare.style.height = "25px";
-        gridSquare.style.width = "25px";
-        gridSquare.style.border = "solid 1px black";
-        gridContainer.appendChild(gridSquare);
+function createGrid (gridSize) {
+    //Loop to create grid based on the input
+    for (let i = 1; i <= gridSize * gridSize; i++) {
+        const gridContainer = document.querySelector("div#grid-container");
+        const gridCell = document.createElement("div");
+        gridContainer.appendChild(gridCell);
+
+        //The square will always be 600x600
+        //If there are fewer squares, they will be bigger.
+        //More squares, they will be smaller.
+        const cellSize = 600 / gridSize;
+        gridCell.setAttribute(`style`, `height: ${cellSize}px; width: ${cellSize}px; border: 0.2px solid black`);
+        gridCell.addEventListener("mouseenter", () => {
+            gridCell.style.backgroundColor = "black";
+        });
     }
 }
 
-createGrid();
+createGrid (16);
